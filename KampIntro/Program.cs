4d0dfeb7a76090de2336f9e3f03de3497da97532
Darga42 <tahirdarga04@gmail.com>
2021-03-25 23:49:13 +0300

    20 Ocak Ders Sonu

---
+++ KampIntro/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself - Kendini tekrar etme 
+            //değer tutucu, alias
+
+            string  kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOranı = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış oku");
+            }
+            else
+            {
+                Console.WriteLine("değişmedi butonu");
+            }
+
+            if (sistemeGirisYapmisMi ==true)
+            {
+                Console.WriteLine("Kullanıcı Ayarlar butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+
+        }
+    }
+}
