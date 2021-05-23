d9d9bacf2817735b9cdf2fd489d3552a3584a612
Darga42 <tahirdarga04@gmail.com>
2021-05-23 16:31:22 +0300

    Kamp 5.Gün

---
+++ SQLQuery2.sql
@@ -0,0 +1,23 @@
+﻿--Select
+--ANSII
+Select ContactName Adi,CompanyName Sirketadi,City Sehir from Customers
+
+Select * from Customers where City = 'Berlin'
+
+--case insensitive
+Select * from Products where CategoryID=1 or CategoryID=3
+
+Select * from Products where CategoryID=1 and UnitPrice>=10
+
+select * from Products where CategoryID=1 order by UnitPrice desc    --ascending  --descending
+
+select count (*) Adet from Products 
+
+select CategoryID,count(*) from Products where UnitPrice>20
+group by CategoryID having count(*)<10
+
+
+
+
+
+
